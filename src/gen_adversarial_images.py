f80fd432ac7babcb72e5ad0d6c2f6a67229f3cc8
nathan13331 <92355141+nathan13331@users.noreply.github.com>
2024-06-23 16:48:48 +0800

    Add files via upload

---
+++ src/gen_adversarial_images.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import argparse
+import torch
+from torch.utils.data import DataLoader
+
+sys.path.insert(1, os.path.abspath(".."))
+from lib import dataset
+from lib.resnet import load_resnet
+from lib.fgsm_attack import  gen_perturbation_file
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate adversarial images for CIFAR-10')
+    parser.add_argument('--model', type=str, default='resnet50',
+                        choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'],
+                        help='Specify which ResNet model to use.')
+
+    return parser.parse_args()
+
+
+def gen_adversarial_images(args):
+    resnet_model = args
+    # Load the saved model
+    test_set = dataset.CIFAR10(f'../pic/test')
+    model = load_resnet(resnet_model)
+    model.load_state_dict(torch.load(f'../pretrained_model/{resnet_model}/original_CIFAR10.pth'))
+    model.eval()
+
+    output_folder = f'../pic/adversarial_images/{resnet_model}'
+    if os.path.exists(output_folder):
+        print(f'Perturbed test set found at {output_folder}')
+        print('Still generating perturbed images? (y/n)')
+        ans = input()
+        if ans.lower() == 'n':
+            exit()
+    else:
+        os.makedirs(output_folder)
+
+    print('Generating perturbed images')
+    test_loader_1 = DataLoader(test_set, batch_size=1, shuffle=True, num_workers=0)
+    gen_perturbation_file(model, test_loader_1, output_folder)
+    print(f'Saved perturbed images to {output_folder}')
+
+
+# run:
+if __name__ == '__main__':
+    args = parse_args()
+    gen_adversarial_images(args.model)
---
+++ src/gen_transform_images.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import torch
+import argparse
+from torch.utils.data import DataLoader
+
+sys.path.insert(1, os.path.abspath(".."))
+from lib import dataset
+from lib.resnet import load_resnet
+from lib.image_transformations import gen_transformed_file
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate transformed images for adversarial attacks')
+    parser.add_argument('--model', type=str, default='resnet18',
+                        choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'],
+                        help='Specify which ResNet model to use.')
+    parser.add_argument('--image_transform', type=str, default='normalized',
+                        help='Specify which image transformation to apply (e.g., blur, noise).')
+
+    return parser.parse_args()
+
+
+def gen_transformed_images(args):
+    resnet_model = args.model
+    image_transform = args.image_transform
+
+    # Load the saved model
+    model = load_resnet(resnet_model)
+    pretrained_model_path = f'../pretrained_model/{resnet_model}/original_CIFAR10.pth'
+    if not os.path.exists(pretrained_model_path):
+        print(f'Model not found at {pretrained_model_path}')
+        return 0
+    else:
+        model.load_state_dict(torch.load(pretrained_model_path))
+        model.eval()
+        print(f'Model loaded from {resnet_model}/original_CIFAR10.pth')
+
+    # Check if the perturbed test set is generated
+    output_folder = f'../pic/adversarial_images/{resnet_model}/'
+    if not os.path.exists(output_folder):
+        print(f'Perturbed test set not found at {output_folder}')
+        return 0
+    else:
+        print(f'Perturbed test set found at {output_folder}')
+
+    # Generate transformed perturbed images if not generated yet
+    output_folder = f'../pic/adversarial_transformed_images/{resnet_model}/'
+    if not os.path.exists(output_folder):
+        print(f'{image_transform} test set not found at {output_folder}')
+        print(f'Generating {image_transform} images')
+        os.makedirs(output_folder)
+        per_test_set = dataset.CIFAR10(f'../pic/adversarial_images/{resnet_model}/')
+        per_test_loader = DataLoader(per_test_set, batch_size=1, shuffle=False, num_workers=0)
+        gen_transformed_file(per_test_loader, output_folder, image_transform)
+        print(f'Saved {image_transform} images to {output_folder}')
+    else:
+        print(f'{image_transform} test set found at {output_folder}')
+        print(f'delete {output_folder} to generate new {image_transform} images')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    gen_transformed_images(args)
